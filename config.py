69caefe92cb03c354815801d8d9892cee91a2856
KENNEDY NDUTHA <91977771+taves-hub@users.noreply.github.com>
2026-02-14 20:41:33 +0300

    Rearrange database URI and track modifications

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
-import os
-
 class Config:
     SECRET_KEY = 'your-secret-key'
+    # PostgreSQL connection string
+    SQLALCHEMY_DATABASE_URI = 'postgresql://sport_equipment_db_user:tTPNuwy5mB9wmGVbZU5kOVYD3A6SCNbJ@dpg-d67e0mbnv86c739mctug-a:5432/sport_equipment_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        SQLALCHEMY_DATABASE_URI = 'postgresql://sport_equipment_db_user:tTPNuwy5mB9wmGVbZU5kOVYD3A6SCNbJ@dpg-d67e0mbnv86c739mctug-a:5432/sport_equipment_db'
-    )

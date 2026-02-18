f5202494d9a3f6c1f94dc3647fb13736c75b845d
KENNEDY NDUTHA <91977771+taves-hub@users.noreply.github.com>
2026-02-18 16:38:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
+
 class Config:
     SECRET_KEY = 'your-secret-key'
     # PostgreSQL connection string
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Taves254@localhost/sports_equipment_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://sport_equipment_db_user:tTPNuwy5mB9wmGVbZU5kOVYD3A6SCNbJ@dpg-d67e0mbnv86c739mctug-a:5432/sport_equipment_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
